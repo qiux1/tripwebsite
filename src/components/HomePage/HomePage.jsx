3c064dd46e3d577265cb515634a9fd808e26ec3a
Xinyu Qiu <qiux1@seattleu.edu>
2023-04-17 11:18:27 -0700

    add a explore now button

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -5,9 +5,18 @@ const HomePage = () => {
     <section className='home'>
       <div className='secContainer container'>
         <div className='homeText'>
+
           <h1 className='title'>
             Plan Your Trip with GoGoGetaway
           </h1>
+
+          <p className='subTitle'>
+            Get ready and plan your travel to your favorite city today!
+          </p>
+
+          <button className='btn'>
+            <a href='#'>Explore now</a>
+          </button>
         </div>
       </div>
     </section>
